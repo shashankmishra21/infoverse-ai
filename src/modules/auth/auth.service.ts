3002f0f1069e9dd2301343fa83ecbb00d3347d24
Shashank Mishra <mishrashashank2106@gmail.com>
2026-01-14 15:18:05 +0000

    authentication part done

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,71 @@
+import prisma from "../../config/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const generateCompanyCode = () => {
+    return "INFV-" + Math.random().toString(36).substring(2, 8).toUpperCase();
+};
+
+//REGISTERR
+export const registerUser = async (name: string, email: string, password: string, companyInput: string) => {
+    const existingUser = await prisma.user.findUnique({ where: { email } });
+    if (existingUser) throw new Error("Email already registered");
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    let company = await prisma.company.findUnique({
+        where: { code: companyInput }
+    });
+
+    let role: "ADMIN" | "USER";
+
+    if (company) {
+        role = "USER";
+    }
+    else {
+        const companyCode = generateCompanyCode();
+
+        company = await prisma.company.create({
+            data: {
+                name: companyInput,
+                code: companyCode,
+            },
+        });
+        role = "ADMIN";
+    }
+
+    const user = await prisma.user.create({
+        data: {
+            name,
+            email,
+            password: hashedPassword,
+            companyId: company.id,
+            role,
+        }
+    });
+    return { user, company }
+}
+
+
+//LOGIN
+export const loginUser = async (email: string, password: string) => {
+    const user = await prisma.user.findUnique({ where: { email } });
+    if (!user) throw new Error("User not found");
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) throw new Error("Invalid Credentials");
+
+    const token = jwt.sign(
+        {
+            userId: user.id,
+            company: user.companyId,
+            role: user.role,
+        },
+        JWT_SECRET as string,
+        { expiresIn: "7d" }
+    );
+
+    return { user, token };
+}
---
+++ src/modules/auth/authController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from "express";
+import { registerUser, loginUser } from "./auth.service";
+
+//Register
+export const register = async (req: Request, res: Response) => {
+    try {
+        const { name, email, password, company } = req.body;
+
+        if (!name || !email || !password || !company) {
+            return res.status(400).json({ error: "All fields required" });
+        }
+
+        const result = await registerUser(name, email, password, company);
+
+        res.status(201).json({
+            message:
+                result.user.role === "ADMIN" ? "Company created. You are ADMIN." : "Joined company successfully.",
+            company: {
+                name: result.company.name,
+                code: result.company.code,
+            },
+            user: result.user,
+        });
+    } catch (err: any) {
+        res.status(400).json({ error: err.message });
+    }
+}
+
+// Login
+export const login = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+        const data = await loginUser(email, password);
+        res.json(data);
+    } catch (err: any) {
+        res.status(401).json({ error: err.message })
+    }
+}
