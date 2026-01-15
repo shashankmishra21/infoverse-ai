46858080397b8aebd0157fa9a520a963c01f79d5
Shashank Mishra <mishrashashank2106@gmail.com>
2026-01-15 17:33:18 +0000

    debug regenerate code

--- src/modules/company/company.controller.ts
+++ src/modules/company/company.controller.ts
@@ -7,7 +7,7 @@ export const reGenerateCode = async (req: AuthRequest, res: Response) => {
     try {
         const companyId = req.user!.companyId;
 
-        
+
 
         if (!companyId) {
             return res.status(400).json({ error: "Company ID missing from token" });
